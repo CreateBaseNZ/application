5fc5167fff7d91a6015d2525493fa11a48ef0e4f
Carl Velasco <carlvelasco96@gmail.com>
2021-01-12 11:19:17 +1300

    First Commit

---
+++ app.js
@@ -0,0 +1,53 @@
+/* ==========================================================
+MODULES
+========================================================== */
+
+const express = require("express");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+
+/* ==========================================================
+VARIABLES
+========================================================== */
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+const app = express();
+
+/* ==========================================================
+SERVER
+========================================================== */
+
+app.listen(process.env.PORT, () => console.log(`Server is running at port ${process.env.PORT}`));
+
+/* ==========================================================
+MIDDLEWARES
+========================================================== */
+
+// Express Middleware: Serve Static Files (HTML, CSS, JS, Images)
+app.use(express.static(__dirname));
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+// Security
+app.use(helmet({ contentSecurityPolicy: false }));
+// X-XSS Header
+app.use((req, res, next) => {
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.removeHeader("X-Powered-By");
+  res.removeHeader("Server");
+  next();
+});
+
+/* ==========================================================
+ROUTERS
+========================================================== */
+
+const generalRouter = require("./routes/general.js");
+app.use(generalRouter);
+
+/* ==========================================================
+END
+========================================================== */
