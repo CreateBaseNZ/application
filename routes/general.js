7024044d62f5eff36df347c24c1fe0146a34b772
Carl Velasco <carlvelasco96@gmail.com>
2021-01-12 13:35:28 +1300

    Routes Updated

--- routes/general.js
+++ routes/general.js
@@ -19,7 +19,32 @@ ROUTES
 // @route   GET /
 // @desc
 // @access  PUBLIC
-router.get("/", (req, res) => res.sendFile("test.html", options));
+router.get("/", (req, res) => res.sendFile("home.html", options));
+
+// @route   GET /dashboard
+// @desc
+// @access  PRIVATE
+router.get("/dashboard", (req, res) => res.sendFile("dashboard.html", options));
+
+// @route   GET /projects
+// @desc
+// @access  PRIVATE
+router.get("/projects", (req, res) => res.sendFile("projects.html", options));
+
+// @route   GET /inbox
+// @desc
+// @access  PRIVATE
+router.get("/inbox", (req, res) => res.sendFile("inbox.html", options));
+
+// @route   GET /orders
+// @desc
+// @access  PRIVATE
+router.get("/orders", (req, res) => res.sendFile("orders.html", options));
+
+// @route   GET /settings
+// @desc
+// @access  PRIVATE
+router.get("/settings", (req, res) => res.sendFile("settings.html", options));
 
 /* ==========================================================
 EXPORT
