9f1ea4a4cb305f9f2a310bdece7ff1a3958fce6b
Brad <bradyoung109gmail.com>
2021-01-22 02:25:25 +1300

    Recover password page progress

---
+++ public/javascripts/recoverpass.js
@@ -0,0 +1,25 @@
+ /*  This is for switching back and forth the input box for user experience */
+ const inputs = document.querySelectorAll('.code-input');
+ for (let i = 0; i < inputs.length; i++) {
+     inputs[i].addEventListener('keydown', function (event) {
+         if (event.key === "Backspace") {
+             inputs[i].value = '';
+             if (i !== 0) {
+                 inputs[i - 1].focus();
+             }
+         } else if (event.key === "ArrowLeft" && i !== 0) {
+             inputs[i - 1].focus();
+         } else if (event.key === "ArrowRight" && i !== inputs.length - 1) {
+             inputs[i + 1].focus();
+         }
+     });
+     inputs[i].addEventListener('input', function () {
+         inputs[i].value = inputs[i].value.toUpperCase(); // Converts to Upper case. Remove .toUpperCase() if conversion isnt required.
+         if (i === inputs.length - 1 && inputs[i].value !== '') {
+             return true;
+         } else if (inputs[i].value !== '') {
+             inputs[i + 1].focus();
+         }
+     });
+
+ }
