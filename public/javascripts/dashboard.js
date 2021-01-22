720e7595bc99889ab518f97b3009f6a61348368d
Louis <clin750@aucklanduni.ac.nz>
2021-01-22 19:25:37 +1300

    Mobile dashboard styling

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -130,24 +130,6 @@ dashboard.loadBadges = () => {
       window.open('/settings', '_self')
     })
   }
-  
-  for (var i = 0; i < 4; i++) {
-    const badge = 'empty';
-    var el = document.createElement('a');
-    el.href = '/settings';
-    el.className = 'badge db-badge ' + badge;
-    var img = document.createElement('img');
-    img.src = '/public/images/badges/' + badge + '.png';
-    el.appendChild(img);
-    var caption = document.createElement('span')
-    caption.innerHTML = badge
-    el.appendChild(caption)
-    dashboard.trophyCase.appendChild(el);
-    img.addEventListener('click', () => {
-      sessionStorage.setItem('dashboard', badge)
-      window.open('/settings', '_self')
-    })
-  }
 }
 
 dashboard.loadProjects = () => {
