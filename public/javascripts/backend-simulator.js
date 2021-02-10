43cc3af6d91c79c6e75e66a646a82b0bb6376b41
Carl Velasco <carlvelasco96@gmail.com>
2021-02-11 09:57:31 +1300

    Backend Simulator

---
+++ public/javascripts/backend-simulator.js
@@ -0,0 +1,30 @@
+const userOne = {
+  name: "Carl Velasco",
+  displayName: "Carl",
+  displayEmail: "carlvelasco96@gmail.com",
+  address: {
+    unit: "",
+    street: "16 Dapple Place",
+    suburb: "Flat Bush",
+    city: "Auckland",
+    postcode: "2019",
+    country: "New Zealand"
+  },
+  location: "Auckland, New Zealand"
+};
+
+const delay = (data = {}, failed = false, error = false, duration = 2000) => {
+  return new Promise(async (resolve, reject) => {
+    setTimeout(() => {
+      if (!error && !failed) {
+        return resolve({ status: "succeeded", content: data });
+      } else {
+        if (failed) {
+          return resolve({ status: "failed", content: "Some failed message" });
+        } else if (error) {
+          return resolve({ status: "error", content: "Some error message" });
+        }
+      }
+    }, duration);
+  })
+}
